84fe07fb132bfc12743873c42cb9947f7b8e21f0
SalenkoDmytro <105796636+SalenkoDmytro@users.noreply.github.com>
2022-11-01 22:31:16 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+1. https://prnt.sc/7opbDIE7SvPp
+2. https://prnt.sc/uxHAz_5W3O_Y
+3. https://prnt.sc/40QRKO3nCfsu
+4. https://prnt.sc/JjgG-LBY_2bm
---
+++ contacts.js
@@ -0,0 +1,59 @@
+const fs = require("fs/promises");
+const path = require("path");
+
+const contactsPath = path.resolve("./db/contacts.json");
+
+async function listContacts() {
+  try {
+    return JSON.parse(await fs.readFile(contactsPath, (err) => {
+      if (err) return err.message;
+    }));
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+async function getContactById(contactId) {
+  try {
+    const contacts = await listContacts();
+    return contacts.filter(({ id }) => id === contactId.toString());
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+async function removeContact(contactId) {
+  try {
+    const contacts = await listContacts();
+    const updatedContacts = contacts.filter(
+      ({ id }) => id !== contactId.toString()
+    );
+    fs.writeFile(contactsPath, JSON.stringify(updatedContacts),, (err) => {
+      if (err) console.error(err);
+    });
+    return updatedContacts;
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+async function addContact(name, email, phone) {
+  try {
+    const contacts = await listContacts();
+    const newContact = {
+      id: (Number(contacts[contacts.length - 1].id) + 1).toString(),
+      name,
+      email,
+      phone,
+    };
+    const newContactsList = [...contacts, newContact];
+    fs.writeFile(contactsPath, JSON.stringify(newContactsList), (err) => {
+      if (err) console.error(err);
+    });
+    return newContactsList;
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+module.exports = { listContacts, getContactById, removeContact, addContact };
